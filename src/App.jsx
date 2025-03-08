32773b356cf99d25b6ead18d12aed193c8e926af
johnmelodev <joaohenrique1231500@gmail.com>
2025-03-08 15:07:24 -0300

    Lists in React map.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,53 @@
-function getTitle(title) {
-  return title;
-}
+import { useState } from 'react';
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello {getTitle('React')}</h1>
+      <h1>My Hacker Stories</h1> 
+      {/* A) O título agora é fixo, não depende mais da função getTitle */}
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
+
+      <hr />
+
+      <ul>
+        {/* B) Um array "list" foi adicionado e está sendo mapeado para criar a lista */}
+        {list.map(function (item) {
+          return (
+            <li key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
 
 export default App;
+
